d8701aba60dc8a2f7f39dfdd1bfc7da92d3933ae
donghun-k <donghun.kdh@gmail.com>
2026-05-12 09:41:00 +0900

    feat(ui): Skeleton 공용 컴포넌트 추가

---
+++ components/ui/skeleton.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { cn } from '@/lib/utils'
+
+export function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn(
+        'animate-pulse rounded-md bg-foreground/5',
+        className
+      )}
+      {...props}
+    />
+  )
+}
